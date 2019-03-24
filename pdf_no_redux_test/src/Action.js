8f2582659caa3997e211e2883a42240eb0df04ad
lujiashen <874497492@qq.com>
2019-03-24 22:23:56 +0800

    add

---
+++ pdf_no_redux_test/src/Action.js
@@ -0,0 +1,14 @@
+
+export const increment = (counterCaption) => {
+    return {
+        type: "INCREMENT",
+        counterCaption: counterCaption
+    }
+}
+
+export const decrement = (counterCaption) => {
+    return {
+        type: "DECREMENT",
+        counterCaption: counterCaption
+    }
+}
